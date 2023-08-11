a29c535b2b63fca756efcacf90834af19f783091
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2023-08-11 15:09:34 +0200

    include files css and js carrousel

--- custom-carrousel.php
+++ custom-carrousel.php
@@ -1,5 +1,24 @@
 <?php
-// Les mêmes fonctions pour charger les fichiers CSS et JS restent inchangées
+function custom_carrousel_admin_enqueue_scripts()
+{
+    wp_enqueue_style('custom-carrousel-admin-styles', plugin_dir_url(__FILE__) . 'css/style-admin.css');
+}
+add_action('admin_enqueue_scripts', 'custom_carrousel_admin_enqueue_scripts');
+
+// Chargement du fichier CSS pour le front-end du site
+function custom_carrousel_enqueue_styles()
+{
+    wp_enqueue_style('custom-carrousel-styles', plugin_dir_url(__FILE__) . 'css/styles.css');
+}
+add_action('wp_enqueue_scripts', 'custom_carrousel_enqueue_styles');
+
+// Chargement du fichier JavaScript pour le front-end du site
+function custom_carrousel_enqueue_scripts()
+{
+    wp_enqueue_script('custom-carrousel-scripts', plugin_dir_url(__FILE__) . 'js/script-carrousel.js', array(), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'custom_carrousel_enqueue_scripts');
+
 
 // Créer une table dans la base de données lors de l'activation du plugin
 function custom_carrousel_create_table()
@@ -29,31 +48,68 @@ register_activation_hook(plugin_dir_path(__FILE__) . 'home-extension.php', 'cust
 function custom_carrousel_shortcode($atts)
 {
     global $wpdb;
-    $atts = shortcode_atts(array('id' => 0), $atts);
     $table_name = $wpdb->prefix . 'custom_carrousel';
 
-    // Requête pour récupérer les éléments du carrousel
-    $items = $wpdb->get_results("SELECT image_url, title, description, link_url FROM $table_name ORDER BY id");
+    // Récupérer toutes les entrées de la table custom_carrousel
+    $items = $wpdb->get_results("SELECT * FROM $table_name");
 
-    // Générer le HTML du carrousel ici en fonction des éléments récupérés
-    // Vous devrez ajuster ce HTML pour correspondre à la structure de votre carrousel
-    $output = '<section id="myCarousel" class="carousel-tablist" aria-roledescription="carousel" aria-label="Highlighted television shows">';
-    $output .= '<div class="carousel-inner">';
+    if (!$items) return 'Aucun élément de carrousel trouvé.';
+
+    $carousel_tabs = '';
+    $carousel_items = '';
+    $count = 1;
+
     foreach ($items as $item) {
-        $output .= '<div class="carousel-item">';
-        $output .= '<div class="carousel-image">';
-        $output .= '<a href="' . esc_url($item->link_url) . '" id="carousel-image-1">';
-        $output .= '<img src="' . esc_url($item->image_url) . '" alt="' . esc_attr($item->description) . '">';
-        $output .= '</a></div>';
-        $output .= '<div class="carousel-caption">';
-        $output .= '<h3><a href="#">' . esc_html($item->title) . '</a></h3>';
-        $output .= '</div></div>';
-    }
-    $output .= '</div></section>';
-    $output .= '<div class="col-sm-1"></div>';
+        $selected = $count === 1 ? 'aria-selected="true"' : 'aria-selected="false"';
+        $active = $count === 1 ? 'active' : '';
 
-    return $output;
+        $carousel_tabs .= '<button id="carousel-tab-' . $count . '" type="button" role="tab" tabindex="-1" aria-label="Slide ' . $count . '" ' . $selected . ' aria-controls="carousel-item-' . $count . '">
+            <svg width="34" height="34" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <circle class="border" cx="16" cy="15" r="10"></circle>
+              <circle class="tab-background" cx="16" cy="15" r="8"></circle>
+              <circle class="tab" cx="16" cy="15" r="6"></circle>
+            </svg>
+          </button>';
+
+        $carousel_items .= '<div class="carousel-item ' . $active . '" id="carousel-item-' . $count . '" role="tabpanel" aria-roledescription="slide" aria-label="' . $count . ' of ' . count($items) . '">
+        <div class="carousel-image">
+          <a href="' . esc_url($item->link_url) . '" id="carousel-image-' . $count . '">
+            <img src="' . esc_url($item->image_url) . '" alt="' . esc_attr($item->title) . '">
+          </a>
+        </div>
+        <div class="carousel-caption">
+          <h3>
+            <a href="' . esc_url($item->link_url) . '"> ' . esc_html($item->title) . ' </a>
+          </h3>
+          <div>
+            <p><span class="contrast">' . esc_html($item->description) . '</span></p>
+          </div>
+        </div></div>';
+
+        $count++;
+    }
+
+    return '<section id="myCarousel" class="carousel-tablist" aria-roledescription="carousel" aria-label="Highlighted television shows">
+        <div class="carousel-inner">
+        <div class="controls">
+        <button class="rotation" type="button">
+        <svg width="42" height="34" version="1.1" xmlns="http://www.w3.org/2000/svg" class="svg-play">
+          <rect class="background" x="2" y="2" rx="5" ry="5" width="38" height="24"></rect>
+          <rect class="border" x="4" y="4" rx="5" ry="5" width="34" height="20"></rect>
+
+          <polygon class="pause" points="17 8 17 20"></polygon>
+
+          <polygon class="pause" points="24 8 24 20"></polygon>
+
+          <polygon class="play" points="15 8 15 20 27 14"></polygon>
+        </svg>
+      </button>
+        <div class="tab-wrapper">
+        <div role="tablist" aria-label="Slides">' . $carousel_tabs . '</div></div></div>
+        <div id="myCarousel-items" class="carousel-items playing" aria-live="off">' . $carousel_items . '</div></div>
+      </section><div class="col-sm-1"></div>';
 }
+
 add_shortcode('custom_carrousel', 'custom_carrousel_shortcode');
 
 // Fonction pour afficher le contenu de la page d'administration du carrousel
@@ -117,4 +173,3 @@ function custom_link_carrousel_page()
     </div>
 <?php
 }
-
